ad505a16ffa830d90b8389bc9e694ee033d0dead
Gaadhe Badal <110352931+gaadhebadal@users.noreply.github.com>
2023-02-22 18:50:28 +0530

    Add files via upload

---
+++ extra/tips.js
@@ -0,0 +1,10 @@
+import"./styles.css";
+
+export default function tips(){
+    return(
+        <div className="tips">
+            <h1>hello codesandox</h1>
+            <P title="hi"></P>
+        </div>
+    );
+}
